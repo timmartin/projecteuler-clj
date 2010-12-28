ff49a2b0fcb8af2e9d81046b29aea18595d0344b
Tim Martin <tim@asymptotic.co.uk>
2010-12-28 14:05:51 +0200

    Added a project dependency on swank-clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject projecteuler-clj "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+				 [swank-clojure "1.2.0"]])
